520dc2b59511c3778873193c81906d5fd896e45f
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-04 15:05:08 +0300

    connected the base and home templates

--- user_story/__init__.py
+++ user_story/__init__.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def base():
-    return render_template('base.html')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
