11a677ca7c53cb99860becbae9992bc81207c665
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-04 15:16:44 +0300

    added fake data to get the app started

--- user_story/__init__.py
+++ user_story/__init__.py
@@ -3,9 +3,31 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+stories = [
+    {
+        'author': 'Yahya Saadi',
+        'title': 'Pickup line',
+        'content': 'Well here I am. What are your other two wishes'
+    },
+    {
+        'author': 'Home',
+        'title': 'Pickup line',
+        'content': "Hey, my name's Microsoft. Can I crash at your place tonight?"
+    },
+    {
+        'author': 'Stewie',
+        'title': 'Pickup line',
+        'content': "Are you French? Because Eiffel for you."
+    }
+]
+
+
+
+
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', stories=stories)
 
 if __name__ == '__main__':
     app.run(debug=True)
