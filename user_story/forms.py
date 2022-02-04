8f270dcc17219d4361d6e73909295f077b8a2cb7
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-04 17:57:28 +0300

    added some corrections

--- user_story/forms.py
+++ user_story/forms.py
@@ -6,13 +6,13 @@ class RegistrationForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    pasword = PasswordField('Password', validators=[DataRequired()])
-    confirm_pasword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Submit')
 
 
 class LoginForm(FlaskForm):
 
     email = StringField('Email', validators=[DataRequired(), Email()])
-    pasword = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
