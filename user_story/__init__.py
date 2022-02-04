1fdd3da6dc86127b26e98120aa4e09f3a43d66b7
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-04 18:01:29 +0300

    made some changes

--- user_story/__init__.py
+++ user_story/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f1c65efed6134c1bc305ffa153496th0'
 
 
 from user_story import routes
