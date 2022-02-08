00d443bc9f398c47fafa86873a3d2d8e4bf0edae
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-08 12:32:43 +0300

    installed some packages and added a link to home page

--- user_story/__init__.py
+++ user_story/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f1c65efed6134c1bc305ffa153496th0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:pass_root@localhost/userstory'
+db = SQLAlchemy(app)
 
 
 from user_story import routes
