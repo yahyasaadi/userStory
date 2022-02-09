0a6a7369bc142d76bc31af0644d6a4c8646a08cd
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-09 10:25:02 +0300

    added some packages

--- user_story/__init__.py
+++ user_story/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f1c65efed6134c1bc305ffa153496th0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:pass_root@localhost/userstory'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from user_story import routes
 
